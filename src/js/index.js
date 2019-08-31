89bf032fa6ae3dfeafa87b44d7344dab948f66b4
armcodes <felixcodesg@gmail.com>
2019-08-31 18:16:28 -0400

    added font-awesome

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 // CSS
 import "../css/main.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
